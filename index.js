1deaf2c2efe17883b4c9488fac93dd6fc787701a
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-11-24 12:43:18 +0600

    user add database

--- index.js
+++ index.js
@@ -2,12 +2,45 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT||5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c8jqjnz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try{
+        const usersCollection=client.db('usedTvBuyAndSell').collection('users');
+
+
+
+
+        app.post('/users',async(req,res)=>{
+            const user=req.body;
+            const result=await usersCollection.insertOne(user);
+            res.send(result)
+      
+          })
+
+
+
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello Worldnbbmbm!')
