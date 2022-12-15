805deb53100a906e84c6d427426c043507bb322f
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-12-15 19:50:14 +0600

    sslcomarze

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const SSLCommerzPayment = require('sslcommerz-lts')
 const app = express()
 const port = process.env.PORT||5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const stripe = require("stripe")(process.env.STRIP_SECRET);
+
 
 
 app.use(cors())
@@ -41,7 +43,9 @@ async function run() {
         const paymentCollection=client.db('usedTvBuyAndSell').collection('payment');
 
 
-
+        const store_id = process.env.SSL_ID;
+        const store_passwd = process.env.STORE_PASS;
+        const is_live = false //true for live, false for sandbox
 
 
         app.post("/create-payment-intent", async (req, res) => {
@@ -62,6 +66,55 @@ async function run() {
 
         
         });
+        app.post('/init', async(req, res) => {
+
+          const order=req.body;
+          const orderService=await productsCollection.findOne({_id:ObjectId(order.productId)});
+          
+
+          // console.log(order);
+          // console.log(orderService);
+          const data = {
+              total_amount: orderService.resalePrice,
+              currency: 'BDT',
+              tran_id: new ObjectId().toString(), // use unique tran_id for each api call
+              success_url: 'http://localhost:3030/success',
+              fail_url: 'http://localhost:3030/fail',
+              cancel_url: 'http://localhost:3030/cancel',
+              ipn_url: 'http://localhost:3030/ipn',
+              shipping_method: 'Courier',
+              product_name: order.product,
+              product_category: 'Electronic',
+              product_profile: 'general',
+              cus_name: order.name,
+              cus_email: order.email,
+              cus_add1: 'Dhaka',
+              cus_add2: 'Dhaka',
+              cus_city: 'Dhaka',
+              cus_state: 'Dhaka',
+              cus_postcode: '1000',
+              cus_country: 'Bangladesh',
+              cus_phone: order.phone,
+              cus_fax: '01711111111',
+              ship_name: 'Customer Name',
+              ship_add1: order.location,
+              ship_add2: 'Dhaka',
+              ship_city: 'Dhaka',
+              ship_state: 'Dhaka',
+              ship_postcode: 1000,
+              ship_country: 'Bangladesh',
+          };
+          // console.log(data);
+          const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+          sslcz.init(data).then(apiResponse => {
+              // Redirect the user to payment gateway
+              let GatewayPageURL = apiResponse.GatewayPageURL
+              res.send({url:GatewayPageURL})
+              
+          });
+      })
+
+
 
         app.post('/paymentstore',async(req,res)=>{
           const payment=req.body;
@@ -70,6 +123,11 @@ async function run() {
 
 
         })
+
+
+// sslc payment
+
+
 //JWT login register
 
 app.get('/jwt',async(req,res)=>{
